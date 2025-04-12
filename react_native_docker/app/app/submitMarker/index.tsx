377f7cee2d87f2a2feb626b4e1213e8515a873f6
Magnus07 <stanislav.pinchuk.02@gmail.com>
2025-04-12 13:28:17 +0300

    Add google maps

---
+++ react_native_docker/app/app/submitMarker/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AddNewMarkerScreen = () => {
+  return (
+    <View>
+      <Text>AddNewMarkerScreen</Text>
+    </View>
+  );
+};
+
+export default AddNewMarkerScreen;
