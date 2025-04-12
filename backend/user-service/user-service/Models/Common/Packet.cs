0f2591a8c7c18335f2c240d1795396550537c6ba
enoreex <icenetovec@gmail.com>
2025-04-12 16:22:50 +0300

    newnew

--- backend/user-service/user-service/Models/Common/Packet.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserService.Models;
-
-public class Packet
-{
-    public Dictionary<string, string> Headers { get; set; } = new();
-
-    public string Body { get; set; } = string.Empty;
-}
