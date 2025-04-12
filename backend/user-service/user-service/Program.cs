2d04a689897d93e8f076cf3dae41ff48c865bb72
enoreex <icenetovec@gmail.com>
2025-04-12 16:50:00 +0300

    fix

--- backend/user-service/user-service/Program.cs
+++ backend/user-service/user-service/Program.cs
@@ -2,6 +2,8 @@ using UserService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 builder.Services.AddControllers();
 builder.Services.AddScoped<ITokenPacketProcessorService, TokenPacketProcessorService>();
 
