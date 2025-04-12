dd5662fea6ea4e093be0dd72e1bfcfba5e838ae7
Олександр <alexbo3605@gmail.com>
2025-04-12 17:50:21 +0300

    Fix conflicts

--- accessability-web/src/App.jsx
+++ accessability-web/src/App.jsx
@@ -1,19 +1,12 @@
 import TableOrganization from './organizations/organizations.table'
 import './App.css'
-import LocationTable from './locations/components/LocationTable'
-import Spliiter from './locations/layouts/Splitter'
-import LocationMap from './locations/components/LocationMap'
 import FromOrganization from './organizations/organizations.form'
-import LocationTable from './locations/components/LocationTable'
-import Spliiter from './locations/layouts/Splitter'
-import LocationMap from './locations/components/LocationMap'
 
 function App() {
-
-
   return (
     <>
-      <Spliiter locationTable={<LocationTable />} locationMap={<LocationMap />} />
+      <FromOrganization />
+      <TableOrganization />
     </>
   )
 }
