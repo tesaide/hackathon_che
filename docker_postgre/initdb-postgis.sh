27810cf257c9c4f93ea77ecaaa1e24692a0f31ea
Oleksandr Halaburda <tesaide@Oleksandrs-MacBook-Air.local>
2025-04-12 13:41:07 +0300

    update postgre

--- docker_postgre/initdb-postgis.sh
+++ docker_postgre/initdb-postgis.sh
@@ -6,20 +6,18 @@ set -e
 export PGUSER="$POSTGRES_USER"
 
 # Create the 'template_postgis' template db
-"${psql[@]}" <<- 'EOSQL'
-CREATE DATABASE template_postgis IS_TEMPLATE true;
-EOSQL
+psql -c "CREATE DATABASE template_postgis IS_TEMPLATE true;"
 
 # Load PostGIS into both template_database and $POSTGRES_DB
 for DB in template_postgis "$POSTGRES_DB"; do
-	echo "Loading PostGIS extensions into $DB"
-	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
-		CREATE EXTENSION IF NOT EXISTS postgis;
-		CREATE EXTENSION IF NOT EXISTS postgis_topology;
-		-- Reconnect to update pg_setting.resetval
-		-- See https://github.com/postgis/docker-postgis/issues/288
-		\c
-		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+    echo "Loading PostGIS extensions into $DB"
+    psql --dbname="$DB" <<-'EOSQL'
+        CREATE EXTENSION IF NOT EXISTS postgis;
+        CREATE EXTENSION IF NOT EXISTS postgis_topology;
+        -- Reconnect to update pg_setting.resetval
+        -- See https://github.com/postgis/docker-postgis/issues/288
+        \c
+        CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+        CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
 done
