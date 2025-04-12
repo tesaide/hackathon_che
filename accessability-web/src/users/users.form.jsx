7fdc67084c2fc8babf9ec5135f12f654ba527d8d
1donato1 <1donato1@github.com>
2025-04-12 16:20:16 +0300

    Add users logic

---
+++ accessability-web/src/users/users.form.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+
+import {
+  Button,
+  Cascader,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Mentions,
+  Segmented,
+  Select,
+  TreeSelect,
+} from 'antd';
+import {MaskedInput} from 'antd-mask-input'
+
+const { RangePicker } = DatePicker;
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 6 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 14 },
+  },
+};
+
+
+
+const UsersForm = () => {
+    const [form] = Form.useForm();
+    const variant = Form.useWatch('outlined', form);
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+      };
+    
+      const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      };
+
+
+
+  return (
+    <Form
+      {...formItemLayout}
+      layout='vertical'
+      form={form}
+      variant={variant || 'outlined'}
+      style={{ maxWidth: 2000, width : 900}}
+      initialValues={{ variant: 'filled' }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+
+
+      <Form.Item  label="ПІБ" name="fullName" rules={[
+        { required: true, message: 'Введіть ПІБ!' },
+        { type: 'string', message: 'Перевірте правильність вводу!'}
+        ]}>
+        <Input placeholder="Прізвище Ім'я Побатькові"/>
+      </Form.Item>
+
+      <Form.Item
+        label="Електронна адреса"
+        name="email"
+        rules={[
+          { required: true, message: 'Введіть вашу електронну адресу!' },
+          { type: 'email', message: 'Перевірте правильність вводу електронної адреси' }
+        ]}
+      >
+        <Input placeholder="example@mail.com" />
+      </Form.Item>
+
+
+      <Form.Item label="Номер телефону" name="phone" rules={[{ required: true, message: 'Введіть ваш номер телефону!' }]}>
+        <MaskedInput mask="+38(\000) 000 00 00"/>
+       </Form.Item>
+
+
+       <Form.Item label= "Роль" name = "role" rules={[{ required: true , message: 'Оберіть вашу роль'}]}>
+       <Select >
+          <Select.Option value="1">
+            Demo
+          </Select.Option>
+          <Select.Option value="2">
+            Demo2
+          </Select.Option>
+          <Select.Option value="3">
+            Demo
+          </Select.Option>
+        </Select>
+        </Form.Item>
+
+        
+        
+        <Form.Item label= "Організація" name = "organization" rules={[{ required: false }]}>
+       <Select >
+       <Select.Option value="null"> </Select.Option>
+          <Select.Option value="1">
+            Demo
+            </Select.Option>
+        </Select>
+        </Form.Item>
+
+
+      
+      <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Зберегти
+        </Button>
+      </Form.Item>
+    </Form >
+  );
+};
+
+export default UsersForm;
