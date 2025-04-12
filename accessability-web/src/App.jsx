9518b6dd91ddc31a3ddb69eaae7c92afd0bd26a2
vanya201 <lenapetrova1905@gmail.com>
2025-04-12 17:39:56 +0300

    feature/locations: refactor

--- accessability-web/src/App.jsx
+++ accessability-web/src/App.jsx
@@ -1,19 +1,16 @@
 import TableOrganization from './organizations/organizations.table'
 import './App.css'
 import LocationTable from './locations/components/LocationTable'
-import Spliiter from './locations/layouts/Splitter'
+import Spliter from './locations/layouts/Splitter'
 import LocationMap from './locations/components/LocationMap'
 import FromOrganization from './organizations/organizations.form'
-import LocationTable from './locations/components/LocationTable'
-import Spliiter from './locations/layouts/Splitter'
-import LocationMap from './locations/components/LocationMap'
 
 function App() {
 
 
   return (
     <>
-      <Spliiter locationTable={<LocationTable />} locationMap={<LocationMap />} />
+      <Spliter locationTable={<LocationTable />} locationMap={<LocationMap />} />
     </>
   )
 }
