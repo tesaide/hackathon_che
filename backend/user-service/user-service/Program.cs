0dcc9433a3fd99e06ccef054a540c74f71d2d843
enoreex <icenetovec@gmail.com>
2025-04-12 20:08:21 +0300

    sry

--- backend/user-service/user-service/Program.cs
+++ backend/user-service/user-service/Program.cs
@@ -1,5 +1,6 @@
 using Services.Database;
 using Services.Token;
+using Services.Users;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.WebHost.UseUrls("http://0.0.0.0:80");
 builder.Services.AddControllers();
 builder.Services.AddScoped<ITokenPacketProcessorService, TokenPacketProcessorService>();
 builder.Services.AddSingleton<IDbService>(new DbService(builder.Configuration.GetConnectionString("Postgres")!));
+builder.Services.AddScoped<UserDataService>();
 
 var app = builder.Build();
 
