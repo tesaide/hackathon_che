e662e10d5974eaa2bd8ecb2cf647790a6e1a8e89
enoreex <icenetovec@gmail.com>
2025-04-13 10:43:20 +0300

    ok cool

--- backend/user-service/user-service/src/Services/Database/Helpers/DbReader.cs
+++ backend/user-service/user-service/src/Services/Database/Helpers/DbReader.cs
@@ -1,24 +1,67 @@
+using System;
 using System.Data;
 
 namespace Services.Database.Helpers;
 
 public static class DbReader
 {
-    public static Guid _GetGuid(this IDataReader r, string name) =>
-        r.GetGuid(r.GetOrdinal(name));
+    public static Guid GetGuid(this IDataReader r, string name)
+    {
+        try
+        {
+            int i = r.GetOrdinal(name);
+            return r.IsDBNull(i) ? Guid.Empty : r.GetGuid(i);
+        }
+        catch { return Guid.Empty; }
+    }
 
-    public static string _GetString(this IDataReader r, string name) =>
-        r.GetString(r.GetOrdinal(name));
+    public static string GetString(this IDataReader r, string name)
+    {
+        try
+        {
+            int i = r.GetOrdinal(name);
+            return r.IsDBNull(i) ? string.Empty : r.GetString(i);
+        }
+        catch { return string.Empty; }
+    }
 
-    public static bool _GetBool(this IDataReader r, string name) =>
-        r.GetBoolean(r.GetOrdinal(name));
+    public static bool GetBool(this IDataReader r, string name)
+    {
+        try
+        {
+            int i = r.GetOrdinal(name);
+            return !r.IsDBNull(i) && r.GetBoolean(i);
+        }
+        catch { return false; }
+    }
 
-    public static byte[] _GetByteArray(this IDataReader r, string name) =>
-        (byte[])r[name];
+    public static byte[] GetByteArray(this IDataReader r, string name)
+    {
+        try
+        {
+            object val = r[name];
+            return val is byte[] data ? data : Array.Empty<byte>();
+        }
+        catch { return Array.Empty<byte>(); }
+    }
 
-    public static DateTime _GetDateTime(this IDataReader r, string name) =>
-        r.GetDateTime(r.GetOrdinal(name));
+    public static DateTime GetDateTime(this IDataReader r, string name)
+    {
+        try
+        {
+            int i = r.GetOrdinal(name);
+            return r.IsDBNull(i) ? DateTime.MinValue : r.GetDateTime(i);
+        }
+        catch { return DateTime.MinValue; }
+    }
 
-    public static DateTime? _GetNullableDateTime(this IDataReader r, string name) =>
-        r.IsDBNull(r.GetOrdinal(name)) ? null : r.GetDateTime(r.GetOrdinal(name));
+    public static DateTime? GetNullableDateTime(this IDataReader r, string name)
+    {
+        try
+        {
+            int i = r.GetOrdinal(name);
+            return r.IsDBNull(i) ? null : r.GetDateTime(i);
+        }
+        catch { return null; }
+    }
 }
