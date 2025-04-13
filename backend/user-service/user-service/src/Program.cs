92e93e66b942e586a227a38a561995b4b533f4c4
enoreex <icenetovec@gmail.com>
2025-04-13 13:59:17 +0300

    OKK CORS

--- backend/user-service/user-service/src/Program.cs
+++ backend/user-service/user-service/src/Program.cs
@@ -6,20 +6,24 @@ using Services.Users;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Слушать на порту 80 (0.0.0.0 — на всех интерфейсах, включая Docker)
 builder.WebHost.UseUrls("http://0.0.0.0:80");
 
+// Настройка CORS: разрешён только конкретный адрес
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .AllowAnyOrigin()
+            .WithOrigins("http://192.168.88.26:25565") // IP Docker-фронта
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
 });
 
 builder.Services.AddControllers();
+
+// DI-сервисы
 builder.Services.AddScoped<ITokenPacketProcessorService, TokenPacketProcessorService>();
 builder.Services.AddSingleton<IDbService>(new DbService(builder.Configuration.GetConnectionString("Postgres")!));
 builder.Services.AddScoped<AddUserService>();
@@ -30,7 +34,7 @@ builder.Services.AddScoped<IPasswordHashingService, PasswordHashingService>();
 
 var app = builder.Build();
 
-app.UseCors();
+app.UseCors(); // подключаем CORS
 
 app.UseRouting();
 
