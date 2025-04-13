c3d957636ebab58fa62006959bb81579878ef3a5
vanya201 <lenapetrova1905@gmail.com>
2025-04-13 11:54:41 +0300

    develop: refactor

--- accessability-web/src/locations/components/LocationForm.jsx
+++ accessability-web/src/locations/components/LocationForm.jsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import {
-  Form, Input, Button, InputNumber, Divider,
-} from 'antd';
 import { useParams } from 'react-router-dom';
 import { MainLayout } from '../../common/layout/MainLayout';
 import { addLocation, updateLocation } from '../actions/locations';
 import { useNavigate } from 'react-router-dom';
 import LocationGoogleMapForm from './LocationGoogleMapForm'
+import { forms } from '../../common/consts';
+import { Select } from 'antd';
 
+const locationTypes = [
+  { label: 'Державна установа', value: 'government_building' },
+  { label: 'Бізнес', value: 'business' },
+  { label: 'Охорона здоров’я', value: 'healthcare' },
+  { label: 'Освіта', value: 'education' },
+  { label: 'Культура', value: 'culture' },
+  { label: 'Транспорт', value: 'transport' },
+  { label: 'Відпочинок', value: 'recreation' },
+  { label: 'Інше', value: 'other' },
+];
+
+
+import { Row, Col, Form, Input, InputNumber, Button, Divider } from 'antd';
 const { TextArea } = Input;
-const googleMapsApiKey = import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-
-const mapContainerStyle = {
-  width: '100%',
-  height: '400px',
-};
 
 const defaultCoordinates = {
   lat: 51.4982,
@@ -63,7 +69,7 @@ const LocationForm = ({ locations, isEditing }) => {
     form.setFieldsValue({ lat, lon });
   }, [form]);
 
-  
+
   const handleSubmit = (values) => {
     const {
       name, address, lat, lon, type, category,
@@ -96,59 +102,79 @@ const LocationForm = ({ locations, isEditing }) => {
 
   return (
     <MainLayout>
-      <h2>{isEditing ? 'Оновити локацію' : 'Створити нову локацію'}</h2>
       <Form form={form} onFinish={handleSubmit} layout="vertical">
-        <Form.Item label="Назва" name="name" rules={[{ required: true, message: 'Введіть назву!' }]}>
-          <Input />
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="Назва"
+              name="name"
+              rules={[{ required: true, message: forms.fieldEmpty }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Тип"
+              name="type"
+              rules={[{ required: true, message: forms.fieldEmpty }]}
+            >
+              <Select
+                options={locationTypes}
+                placeholder="Оберіть тип"
+                allowClear
+              />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="Адреса" name="address" rules={[{ required: true, message: 'Введіть адресу!' }]}>
-          <Input />
-        </Form.Item>
 
-        <Form.Item label="Тип" name="type" rules={[{ required: true, message: 'Введіть тип локації!' }]}>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Категорія" name="category" rules={[{ required: true, message: 'Введіть категорію!' }]}>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Опис" name="description" rules={[{ required: true, message: 'Введіть опис!' }]}>
+        <Form.Item label="Опис" name="description" rules={[{ required: true, message: forms.fieldEmpty }]}>
           <TextArea rows={3} />
         </Form.Item>
 
-        <Form.Item label="Телефони (через кому)" name="phones" rules={[{ required: true, message: 'Введіть телефони!' }]}>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item label="Телефони (через кому)" name="phones" rules={[{ required: true, message: forms.fieldEmpty }]}>
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Email (через кому)" name="emails" rules={[{ required: true, message: forms.fieldEmpty }]}>
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Form.Item label="Адреса" name="address" rules={[{ required: true, message: forms.fieldEmpty }]}>
           <Input />
         </Form.Item>
 
-        <Form.Item label="Email (через кому)" name="emails" rules={[{ required: true, message: 'Введіть email!' }]}>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Робочий час" name="working_hours" rules={[{ required: true, message: 'Введіть робочий час!' }]}>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Координати (широта, довгота)" required>
-          <Form.Item name="lat" noStyle rules={[{ required: true, message: 'Введіть широту!' }]}>
-            <InputNumber style={{ width: '48%' }} placeholder="Широта" />
-          </Form.Item>
-          <Form.Item name="lon" noStyle rules={[{ required: true, message: 'Введіть довготу!' }]}>
-            <InputNumber style={{ width: '48%', marginLeft: '4%' }} placeholder="Довгота" />
-          </Form.Item>
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item label="Широта" name="lat" rules={[{ required: true, message: forms.fieldEmpty }]}>
+              <InputNumber style={{ width: '100%' }} placeholder="Широта" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Довгота" name="lon" rules={[{ required: true, message: forms.fieldEmpty }]}>
+              <InputNumber style={{ width: '100%' }} placeholder="Довгота" />
+            </Form.Item>
+          </Col>
+        </Row>
 
         <Divider />
-            <LocationGoogleMapForm
-                lat={coordinates.lat}
-                lon={coordinates.lon}
-                onClick={handleMapClick}
-            />
+
+        <LocationGoogleMapForm
+          lat={coordinates.lat}
+          lon={coordinates.lon}
+          onClick={handleMapClick}
+        />
+
         <Divider />
 
         <Button type="primary" htmlType="submit">
-          {isEditing ? 'Оновити локацію' : 'Додати локацію'}
+          {forms.save}
         </Button>
       </Form>
     </MainLayout>
