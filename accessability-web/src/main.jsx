867494b58c2550a522ef23c9cada120835367ac9
Serge Zenchenko <sszwrk@gmail.com>
2025-04-13 12:27:22 +0300

    Fixed import of UsersForm.

--- accessability-web/src/main.jsx
+++ accessability-web/src/main.jsx
@@ -4,9 +4,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UsersTable from './users/users.table';
+import UsersForm from './users/users.form';
 import RolesTable from './roles/roles.table';
-import OrganizationsTable from './organizations/OrganizationsTable.jsx';
-import OrganizationForm from './organizations/OrganizationsForm.jsx';
+import OrganizationsTable from './organizations/OrganizationsTable';
+import OrganizationForm from './organizations/OrganizationsForm';
 import LocationTable from './locations/components/LocationTable';
 import AccessibilityFeaturesTable from './accessibilityFeatures/AccessibilityFeaturesTable';
 import LocationFormWrapper from './locations/components/LocationFormWrapper';
@@ -25,8 +26,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/roles" element={<RolesTable />} />
         <Route path="/organizations" element={<OrganizationsTable />} />
 
-        <Route path="/organizations/create" element={<OrganizationForm  />} />
-        <Route path="/organizations/:id" element={<OrganizationForm  />} />
+        <Route path="/organizations/create" element={<OrganizationForm />} />
+        <Route path="/organizations/:id" element={<OrganizationForm />} />
         <Route path="/locations" element={<LocationTable />} />
         <Route path="/locations/create" element={<LocationFormWrapper />} />
         <Route path="/locations/update/:id" element={<LocationFormWrapper />} />
